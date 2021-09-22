ee9abaa6c5ba28808114fdd8d3a17044c2766d0a
joorjin <45465458+joorjin@users.noreply.github.com>
2021-09-22 23:44:03 +0330

    cheange all style (#8)
    * fix conflicts 1
    
    * ver = 3.0.0
    
    * ver = 1.0.2 , ver = 4.0.0
    
    * ver = 3.1.0
    
    * Half version
    
    * nim ver
    
    * add ver
    
    * add ver
    
    * test git push
    
    * add git pull
    
    * test pull
    
    * test pull 2
    
    * cheange all style

---
+++ resources/views/part2.blade.php
@@ -0,0 +1,24 @@
+<div class="part2">
+    <img class="backgruond" src="img/02-h-02.png" alt="">
+    <img class="caracter" src="img/Caracter-miror.png" alt="">
+
+    <div class="texts">
+      <div class="text1">
+        <p>امتیاز شما :25</p>
+      </div>
+      <div class="text2">
+        <b>جوایز من</b>
+        <p></p>
+        @foreach ($award_wonss as $item)
+            <p><?php echo($item[0]['name']); ?></p>
+        @endforeach
+      </div>
+
+      <div class="text3">
+        <p>لینک دعوت شما :
+          <br>
+          <b>https://20bash.bistbit.com/{{ $invitation_code }}</b></p>
+      </div>
+    </div>
+  </div>
+  <!-- end part 2 -->
---
+++ resources/views/part3.blade.php
@@ -0,0 +1,55 @@
+<div class="part3">
+
+    <!-- The Modal -->
+<div id="myModal" class="modal">
+
+<!-- Modal content -->
+<div class="modal-content">
+  <span class="close">&times;</span>
+
+  <p>ابتدا شماره خود را وارد کنید.</p>
+  <form action="add_phone" method="post" class="register">
+      @csrf
+      <input type="tel" pattern="[0-9]{11}" name="phone" placeholder="شماره شما">
+      <input type="password" name="pass" id="pass" placeholder="یک پسورد برای خود انتخاب کنید" minlength="8">
+      <button class="btn btn-success"> بزن بریم! </button>
+  </form>
+
+</div>
+
+</div>
+
+
+    <div class="spin-loc">
+      <div class="Triangle"></div>
+ <!-- Hero section -->
+ <section class="spin-section">
+    <div class="row">
+      <div class="col-md-0 col-lg-1"></div>
+      <div class="col-md-12 col-lg-11 hero-text">
+        <!-- <h2>با تولید سادگی نامفهوم از صنعت چاپ <span>بیت‌کوین</span> <br>با تولید سادگی نامفهوم از صنعت چاپ</h2> -->
+        <figure class="spin">
+            <!-- {{-- <div onclick="spin()" style="width: 10px; height: 10px;background: #4989;"></div> --}} -->
+            <div id="spin-location"></div>
+            <img onclick="searchAward()" id="nologin" src="img/WheelOfFortune.png" class="spinImg" alt="">
+        </figure>
+        <p id="show-award"></p>
+      </div>
+    </div>
+  </section>
+
+<!-- Hero section end -->
+    </div>
+    <div class="text">
+        <h2>گردونه شانس!</h2>
+        <b>نکته:</b>
+        <p>توجه داشته باشید که شما در
+            <br> هر هفته تا پایان کمپین فرصت
+            <br>گرداندن چرخونه برای در یافت
+            <br> جوایز بیشتر خواهید داشت.
+            <br><br>با دعوت از دوستانتان میتوانید
+            <br>تعداد دفعات بیشتری اقدام  به
+            <br>گراندن چرخونه نمایید
+        </p>
+    </div>
+</div>
