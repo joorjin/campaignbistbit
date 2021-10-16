0600bcd58c0d4010c6c01f2fe30f1bd6823d797a
joorjin <joorjin2@gmail.com>
2021-10-16 20:00:48 +0330

    fix style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -138,6 +138,7 @@
 
             setTimeout(() => {
                 $(".Caracter").css('display', 'inline');
+                $(".Caracter").css('display', 'inline');
                 $(".Caracter").animate({marginTop:'-20%'},2000);
             }, 1000);
                 $(".Caracter").animate({marginTop:'-20%'},2000);
